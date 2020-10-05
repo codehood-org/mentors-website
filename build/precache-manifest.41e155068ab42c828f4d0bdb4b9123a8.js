d2330f4ba8dfabb940056f792ea3af60354acc6c
Dr Islam Sayed_Rep_Phs <islam.sayed@elezabypharmacy.com>
2020-10-05 13:33:45 +0200

    commit the build dir

---
+++ build/precache-manifest.41e155068ab42c828f4d0bdb4b9123a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898fcbaf779a76d2985ebea39c0164a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d8eba97201e266a9502",
+    "url": "/static/css/main.79850b8b.chunk.css"
+  },
+  {
+    "revision": "50b0ae7ee99521a14b5e",
+    "url": "/static/js/2.98cbe187.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.98cbe187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8eba97201e266a9502",
+    "url": "/static/js/main.a9edce06.chunk.js"
+  },
+  {
+    "revision": "84dcdfdbe8bd593cf8ba",
+    "url": "/static/js/runtime-main.6bf52bc1.js"
+  }
+]);
