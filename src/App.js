3d7691aa1c5c5a41ad2ef0c0c6a66edcd5a40842
Dr Islam Sayed_Rep_Phs <islam.sayed@elezabypharmacy.com>
2020-10-05 20:28:41 +0200

    add MentorFilter props and functions to filter mentors

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import MentorFilter from "./controllers/mentor-filter/MentorFilter";
 import mentorsList from "./data/mentors.json";
 
 //Modify the default Mui Theme
-const globalTheme = createMuiTheme({
-
-});
+const globalTheme = createMuiTheme({});
 
 function App() {
   const [mentors, setMentors] = useState(mentorsList);
+
+  const filterBySkill = (skill) => setMentors(mentorsList.filter(mentor => mentor.skills.contain(skill)))
+  const filterByCountry = (country) => setMentors(mentorsList.filter(mentor => mentor.country === country))
+
   return (
     <div className="App">
       <ThemeProvider theme={globalTheme}>
         <h1>Mentors Website</h1>
         <Grid container spacing={2}>
           <Grid container item md={2}>
-            <MentorFilter mentors={mentors} />
+            <MentorFilter
+              mentors={mentors}
+              filterBySkill={filterBySkill}
+              filterByCountry={filterByCountry}
+            />
           </Grid>
           <Grid container item md={10} spacing={2} justify="flex-start">
             {mentors.map((mentor, index) => (
