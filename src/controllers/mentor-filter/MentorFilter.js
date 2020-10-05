cb99f85735258467e554a2353e997a428824c450
Islam Sayed <islam.sayed8@gmail.com>
2020-10-05 22:50:39 +0200

    add Grids to TextFields and fullWidth

--- src/controllers/mentor-filter/MentorFilter.js
+++ src/controllers/mentor-filter/MentorFilter.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Grid } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
+/* const useStyles = makeStyles((theme) => ({
   root: {
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: "25ch",
     },
   },
+})); */
+
+const useStyles = makeStyles(() => ({
+
 }));
 
 const arabCountries = {
@@ -45,59 +49,63 @@ const MentorFilter = (props) => {
 
   const handleSkillSelection = (e) => {
     setSkill(e.target.value);
-    filterBySkill(e.target.value)
-  } 
+    filterBySkill(e.target.value);
+  };
   const handleCountrySelection = (e) => {
     setCountry(e.target.value);
-    filterByCountry(e.target.value)
-  } 
+    filterByCountry(e.target.value);
+  };
   //creating a unique skills array.
   const skills = [...new Set(mentors.map((mentor) => mentor.skills).flat())];
   const filteredCountries = [
     ...new Set(mentors.map((mentor) => mentor.countryAlpha2Code)),
   ];
-  console.log(filteredCountries);
   const countries = filteredCountries.map((country) => {
     return {
       value: country,
       label: arabCountries[country],
     };
   });
-  console.log(countries);
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        {/*FILTER BY SKILLS */}
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Skill"
-          value={skill}
-          onChange={handleSkillSelection}
-          helperText="filter mentors by skills"
-        >
-          {skills.map((skill) => (
-            <MenuItem key={skill} value={skill}>
-              {skill}
-            </MenuItem>
-          ))}
-        </TextField>
-        {/*FILTER BY COUNTRY */}
-        <TextField
-          id="standard-select-currency"
-          select
-          label="country"
-          value={country}
-          onChange={handleCountrySelection}
-          helperText="filter mentors by country"
-        >
-          {countries.map((country) => (
-            <MenuItem key={country.value} value={country.value}>
-              {country.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
+      <Grid container>
+        <Grid item xs={12}>
+          {/*FILTER BY SKILLS */}
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Skill"
+            value={skill}
+            onChange={handleSkillSelection}
+            helperText="filter mentors by skills"
+            fullWidth
+          >
+            {skills.map((skill) => (
+              <MenuItem key={skill} value={skill}>
+                {skill}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="standard-select-currency"
+            select
+            label="country"
+            value={country}
+            onChange={handleCountrySelection}
+            helperText="filter mentors by country"
+            fullWidth
+          >
+            {countries.map((country) => (
+              <MenuItem key={country.value} value={country.value}>
+                {country.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
+      {/*FILTER BY COUNTRY */}
     </form>
   );
 };
