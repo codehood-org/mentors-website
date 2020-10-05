0cab11a6038ccaa57e0f194ab7a1baa9630b450b
Dr Islam Sayed_Rep_Phs <islam.sayed@elezabypharmacy.com>
2020-10-05 15:43:24 +0200

    import createMuiTheme, ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Grid } from "@material-ui/core";
+import { Grid, createMuiTheme, ThemeProvider } from "@material-ui/core";
 import MentorCard from "./controllers/mentor-card/MentorCard";
 import MentorFilter from "./controllers/mentor-filter/MentorFilter";
-import mentorsList from "./data/mentors.json"
+import mentorsList from "./data/mentors.json";
+
+//Modify the default Mui Theme
+const globalTheme = createMuiTheme({
+
+});
 
 function App() {
-  const [mentors, setMentors] = useState(mentorsList)
-
+  const [mentors, setMentors] = useState(mentorsList);
   return (
     <div className="App">
-      <h1>Mentors Website</h1>
-      <Grid container spacing={2}>
-        <Grid container item  md={2}>
-          <MentorFilter mentors={mentors} />
+      <ThemeProvider theme={globalTheme}>
+        <h1>Mentors Website</h1>
+        <Grid container spacing={2}>
+          <Grid container item md={2}>
+            <MentorFilter mentors={mentors} />
+          </Grid>
+          <Grid container item md={10} spacing={2} justify="flex-start">
+            {mentors.map((mentor, index) => (
+              <Grid key={index} item>
+                <MentorCard mentor={mentor} />
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
-        <Grid container item  md={10} spacing={2} justify="flex-start">
-          {mentors.map((mentor, index) => (
-            <Grid key={index} item>
-              <MentorCard mentor={mentor} />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
+      </ThemeProvider>
     </div>
   );
 }
