f33c47ca49ac33c3acf77877d66f3dd7ffa03f64
Islam Sayed <islam.sayed8@gmail.com>
2020-10-05 06:59:50 +0200

    import MentorFilter comp, mentorList json, add mentors state

--- src/App.js
+++ src/App.js
@@ -1,82 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Grid } from "@material-ui/core";
 import MentorCard from "./controllers/mentor-card/MentorCard";
+import MentorFilter from "./controllers/mentor-filter/MentorFilter";
+import mentorsList from "./data/mentors.json"
 
 function App() {
-  const mentors = [
-    {
-      name: "Islam Sayed",
-      title: "full-stack developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: ["html", "css", "javascript"],
-      country: "EG",
-      image: "https://uifaces.co/our-content/donated/ukegoVAy.jpg",
-    },
-    {
-      name: "Mohammed Riad",
-      title: ".Net developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: [".net", "c#", "javascript"],
-      country: "SA",
-      image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTk2OGU4NzktODhhOC00Nzc2LWIyNzYtOWViMjljZGFiNTMxXkEyXkFqcGdeQXVyMTE1NTQwOTk@256_AL_.jpg",
-    },
-    {
-      name: "Islam Sayed",
-      title: "full-stack developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: ["html", "css", "javascript"],
-      country: "EG",
-      image: "https://uifaces.co/our-content/donated/ukegoVAy.jpg",
-    },
-    {
-      name: "Mohammed Riad",
-      title: ".Net developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.I can help you answer the "why" questions and explain difficuilt systems in simple terms.I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: [".net", "c#", "javascript"],
-      country: "SA",
-      image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTk2OGU4NzktODhhOC00Nzc2LWIyNzYtOWViMjljZGFiNTMxXkEyXkFqcGdeQXVyMTE1NTQwOTk@._V1_UY256_CR12,0,172,256_AL_.jpg",
-    },
-    {
-      name: "Islam Sayed",
-      title: "full-stack developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: ["html", "css", "javascript"],
-      country: "EG",
-      image: "https://uifaces.co/our-content/donated/ukegoVAy.jpg",
-    },
-    {
-      name: "Mohammed Riad",
-      title: ".Net developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: [".net", "c#", "javascript"],
-      country: "SA",
-      image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTk2OGU4NzktODhhOC00Nzc2LWIyNzYtOWViMjljZGFiNTMxXkEyXkFqcGdeQXVyMTE1NTQwOTk@._V1_UY256_CR12,0,172,256_AL_.jpg",
-    },
-    {
-      name: "Mohammed Riad",
-      title: ".Net developer",
-      message:
-        'I can help you answer the "why" questions and explain difficuilt systems in simple terms.I can help you answer the "why" questions and explain difficuilt systems in simple terms.I can help you answer the "why" questions and explain difficuilt systems in simple terms.',
-      skills: [".net", "c#", "javascript"],
-      country: "SA",
-      image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTk2OGU4NzktODhhOC00Nzc2LWIyNzYtOWViMjljZGFiNTMxXkEyXkFqcGdeQXVyMTE1NTQwOTk@._V1_UY256_CR12,0,172,256_AL_.jpg",
-    },
-  ];
+  const [mentors, setMentors] = useState(mentorsList)
+
   return (
     <div className="App">
       <h1>Mentors Website</h1>
-      <Grid container>
-        <Grid container item>
-          
+      <Grid container spacing={2}>
+        <Grid container item  md={2}>
+          <MentorFilter mentors={mentors} />
         </Grid>
-        <Grid container item spacing={3} justify="flex-start">
+        <Grid container item  md={10} spacing={2} justify="flex-start">
           {mentors.map((mentor, index) => (
             <Grid key={index} item>
               <MentorCard mentor={mentor} />
