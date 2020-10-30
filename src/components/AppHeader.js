c06fb50a37c35ee988dd4e91beac7e47b1bed190
Mohamed Najiub <mohamednajiub@gmail.com>
2020-10-30 18:42:45 +0200

    add codehood logo in header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import {
   Container,
+  Grid,
   AppBar,
   Toolbar,
   FormControlLabel,
   Switch,
   Typography,
   makeStyles,
+  Link as MUILink,
+  Hidden
 } from "@material-ui/core";
 
 import DarkIcon from "@material-ui/icons/Brightness4";
 import LightIcon from "@material-ui/icons/Brightness7";
 
+import CodeHoodLogo from '../images/codehood-logo.jpg'
+
 const useStyles = makeStyles((theme) => ({
-  title: {
-    flexGrow: 1,
-  },
-  
-  containerMain: {
-    paddingTop: theme.spacing(4),
-  },
+
+    brand: {
+        display: 'flex',
+        color: '#fff',
+        alignItems: 'center',
+        
+        '& img': {
+            display: 'block',
+            width: '60px',
+            height: '60px'
+        }
+    },
+
+    toolbar_container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
 }));
 
 const AppHeader = ({darkMode, ModeChange}) => {
@@ -28,15 +44,20 @@ const AppHeader = ({darkMode, ModeChange}) => {
     return (
         <AppBar position="static" className={classes.appBar}>
             <Container maxWidth='lg'>
-                <Toolbar disableGutters>
-                    
-                    <Typography
-                    variant={window.innerWidth < 637 ? "h6" : "h4"}
-                    className={classes.title}
-                    >
-                        Codehood Mentors
-                    </Typography>
-                    
+                <Toolbar disableGutters className={classes.toolbar_container}>
+
+                    <MUILink href="/" title="CodeHood Mentors" className={classes.brand}>
+                        <img src={CodeHoodLogo} alt="Codehood" />
+                        <Hidden only={['xs', 'sm']}>
+                            <Typography
+                                variant={window.innerWidth < 637 ? "h6" : "h4"}
+                                className={classes.title}
+                            >
+                                Mentors
+                            </Typography>
+                        </Hidden>
+                    </MUILink>
+
                     <FormControlLabel
                         control={
                             <Switch
@@ -54,9 +75,8 @@ const AppHeader = ({darkMode, ModeChange}) => {
                                 <DarkIcon htmlColor='#ffffff' />
                             )
                         }
-
                     />
-
+                    
                 </Toolbar>
             </Container>
         </AppBar>
